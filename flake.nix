059e1b9dce9d3b59aa8649ed51b63fb4a1832761
Shubhanshu Tomar <57960301+tomshoo@users.noreply.github.com>
2023-07-10 15:09:47 +0530

    feat[reproducability]: add nix flake configuration
    Nix flakes can help in making precisely reproducable build environments
    and can be combined with docker to provide even more flexible
    environments.
    
    It also includes a `.direnv` file to allow automatically setting up the
    shell environment without hiccups if both [`nix`](https://nixos.org/)
    and [`direnv`](https://github.com/direnv/direnv) are setup.

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{ description = "";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { flake-utils,
+              rust-overlay,
+              nixpkgs,
+              ...
+  }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {
+      overlays = [ (import rust-overlay) ];
+      localSystem.system = system;
+    };
+  in
+  { devShells.default = pkgs.mkShell {
+      nativeBuildInputs = [ (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml) ];
+      buildInputs = with pkgs; [ qemu cargo-bootimage ];
+    };
+  });
+}
